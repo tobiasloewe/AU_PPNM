3cc78ca79915db73dd9c5df1354f6f0927998c29
tobiasloewe <tobias.loewe@studium.uni-hamburg.de>
2025-01-29 11:00:29 +0100

    gamma up to 10 works

--- exercises/math/math.cs
+++ exercises/math/math.cs
@@ -5,7 +5,9 @@ public class math{
    public static void testSomeMath(){
       double sqrt2=Sqrt(2.0);
       Write($"sqrt2^2 = {sqrt2*sqrt2} (should equal 2)\n");
-      Write($"Gamma(1) = {sfuns.fgamma(1.0)}\n");
-      Write($"Gamma(3) = {sfuns.fgamma(3.0)}\n");
+      for (double i=1; i<11.0; i++){
+         Write($"Gamma({i}) = {sfuns.fgamma(i)}\n");
+      }
+
    }
 }
