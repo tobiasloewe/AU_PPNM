d9bce0ddf817faa560c461eac3f6b881392b3ee0
tobiasloewe <tobias.loewe@studium.uni-hamburg.de>
2025-01-29 08:55:19 +0100

    WIP branch math, library not functioning yet, printout incorrect

---
+++ exercises/math/sfuns.cs
@@ -0,0 +1,11 @@
+using static System.Math;
+
+class sfuns{
+   public static double fgamma(double x){
+   ///single precision gamma function (formula from Wikipedia)
+      if(x<0)return PI/Sin(PI*x)/fgamma(1-x); // Euler's reflection formula
+      if(x<9)return fgamma(x+1)/x; // Recurrence relation
+      double lnfgamma=x*Log(x+1/(12*x-1/x/10))-x+Log(2*PI/x)/2;
+      return Exp(lnfgamma);
+   }
+}
