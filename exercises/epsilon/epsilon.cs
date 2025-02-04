9c4e01871c57b2bc2e4a83bc8597a1071b93f54a
tobiasloewe <tobias.loewe@studium.uni-hamburg.de>
2025-02-04 21:15:26 +0100

    still not running, too slow or so

--- exercises/epsilon/epsilon.cs
+++ exercises/epsilon/epsilon.cs
@@ -8,13 +8,13 @@ public static class epsilon{
     }
     public static double testMinDouble(){
         double x = 1;
-        while(x+1!=x){
+        while((x+1)!=x){
             x/=2;
         }
         x*=2;
         return x;
     }
-    public static double testMinFloat(){
+    public static float testMinFloat(){
         float y=1F; 
         while((float)(1F+y) != 1F){
             y/=2F;
--- exercises/epsilon/main.cs
+++ exercises/epsilon/main.cs
@@ -1,11 +1,15 @@
 class main{
 static int Main(){
+        int myMaxInt = epsilon.testMaxInt();
         System.Console.WriteLine($"Int.MaxValue = {int.MaxValue}");
-		System.Console.WriteLine($"My max int: {epsilon.testMaxInt()}");
-
-        System.Console.WriteLine($"My machine epsilon for float: {epsilon.testMinDouble()}");
+		System.Console.WriteLine($"My max int: {myMaxInt}");
+        
+        double myMinDouble = epsilon.testMinDouble();
+        System.Console.WriteLine($"My machine epsilon for double: {myMinDouble}}");
         System.Console.WriteLine($"Compare with 2^(-52) = {System.Math.Pow(2,-52)}");
-        System.Console.WriteLine($"My machine epsilon for double: {epsilon.testMinFloat()}");
+        
+        float myMinFloat = epsilon.testMinFloat();
+        System.Console.WriteLine($"My machine epsilon for float: {myMinFloat}");
         System.Console.WriteLine($"Compare with 2^(-23) = {System.Math.Pow(2,-23)}");
 		return 0;
 	}
