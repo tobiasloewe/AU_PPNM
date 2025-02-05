1db4e1e35465345cdc404faa93ee81eb5a6a00a8
tobiasloewe <tobias.loewe@studium.uni-hamburg.de>
2025-02-05 14:20:54 +0100

    done I think

--- exercises/epsilon/epsilon.cs
+++ exercises/epsilon/epsilon.cs
@@ -1,7 +1,14 @@
 using System;
+using static System.Console;
+using static System.Math;
 
 public class epsilon
 {
+    public static bool approx(double a, double b, double acc=1e-9, double eps=1e-9){
+        if(Abs(b-a) <= acc) return true;
+        if(Abs(b-a) <= Max(Abs(a),Abs(b))*eps) return true;
+        return false;
+    }
     // Find the largest integer before overflow
     public static int testMaxInt()
     {
@@ -34,4 +41,26 @@ public class epsilon
         }
         return epsilon * 2.0F;
     }
+    public static void tinyTest(){
+        double epsilon=Pow(2,-52);
+        double tiny=epsilon/2;
+        double a=1+tiny+tiny;
+        double b=tiny+tiny+1;
+        Write($"a==b ? {a==b}\n");
+        Write($"a>1  ? {a>1}\n");
+        Write($"b>1  ? {b>1}\n");
+        WriteLine($"tiny = {tiny}\nepsilon = {epsilon}\ntiny/2 = {tiny/2}");
+        WriteLine($"a = {a} and b = {b}");
+    }
+    public static void doubleComparisons(){
+        double d1 = 0.1+0.1+0.1+0.1+0.1+0.1+0.1+0.1;
+        double d2 = 8*0.1; 
+        WriteLine($"d1={d1:e15}");
+        WriteLine($"d2={d2:e15}");
+        WriteLine($"d1==d2 ? => {d1==d2}"); 
+        WriteLine("0.1 cannot be accurately represented");
+        WriteLine($"using new approx function for comparison we get: {approx(d1,d2)}");
+    }
+    
 }
+
--- exercises/epsilon/main.cs
+++ exercises/epsilon/main.cs
@@ -13,6 +13,13 @@ static int Main(){
         float myMinFloat = epsilon.testMinFloat();
         WriteLine($"My machine epsilon for float: {myMinFloat}");
         WriteLine($"Compare with 2^(-23) = {(float)System.Math.Pow(2,-23)}");
+        
+        WriteLine("\nTesting with tiny=epsilon/2");
+        epsilon.tinyTest();
+        WriteLine("By definition, tiny is half the machine epsilon, which is the smallest difference from 1.0, that\nis representable. Adding half doesn't change anything, also addition is associative.");
+    
+        WriteLine("\nNow onto double comparisons");
+        epsilon.doubleComparisons();
 		return 0;
 	}
 }
