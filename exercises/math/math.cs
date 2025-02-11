d4379524af013b7eb476f04cb96842bf95d920cc
tobiasloewe <tobias.loewe@studium.uni-hamburg.de>
2025-02-11 10:22:10 +0100

    completion of this exercise I think

--- exercises/math/math.cs
+++ exercises/math/math.cs
@@ -4,7 +4,14 @@ using static System.Math;
 public class math{
    public static void testSomeMath(){
       double sqrt2=Sqrt(2.0);
-      Write($"sqrt2^2 = {sqrt2*sqrt2} (should equal 2)\n");
+      WriteLine($"sqrt2^2 = {sqrt2*sqrt2} (should equal 2)");
+      double twoToOneFifth = Pow(2,0.2);
+      WriteLine($"2^(1/5) = {twoToOneFifth} (should be 1.148698)");
+      double eToPi = Pow(E,PI);
+      WriteLine($"e^pi = {eToPi} (should be 23.140692)");
+      double piToE = Pow(PI,E);
+      WriteLine($"pi^e = {piToE} (should be 22.459158)");
+
       for (double i=1; i<11.0; i++){
          Write($"Gamma({i}) = {sfuns.fgamma(i):F6}\n");
       }
