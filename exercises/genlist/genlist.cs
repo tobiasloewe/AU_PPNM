671fc24cdc83964b55513c67211992b60abc81bd
tobiasloewe <tobias.loewe@studium.uni-hamburg.de>
2025-02-13 17:18:28 +0100

    new class for better genlist started

--- exercises/genlist/genlist.cs
+++ exercises/genlist/genlist.cs
@@ -10,7 +10,7 @@ public class genlist<T>{
 		data=newdata;
 	}
 	public void remove(int i){
-		T[] newdata = new T[size - 1];
+		T[] newdata = new T[size - 1];	
 		for (int j = 0; j<size-1;j++){
 			if (j<i){
 				newdata[j] = data[j];
@@ -20,5 +20,21 @@ public class genlist<T>{
 			}
 		}
 		data = newdata;
+	}
+
+}
+
+public class genlistbetter<T>{
+	public T[] data;
+	public int size=0,capacity=8;
+	public genlist(){ data = new T[capacity]; }
+	public void add(T item){ /* add item to list */
+		if(size==capacity){
+			T[] newdata = new T[capacity*=2];
+			System.Array.Copy(data,newdata,size);
+			data=newdata;
+			}
+		data[size]=item;
+		size++;
 	}
 }
