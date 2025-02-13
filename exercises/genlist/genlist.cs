26ac586952382b3eaa269e5b0968068f82b4a13c
tobiasloewe <tobias.loewe@studium.uni-hamburg.de>
2025-02-13 16:55:26 +0100

    progress

--- exercises/genlist/genlist.cs
+++ exercises/genlist/genlist.cs
@@ -3,10 +3,23 @@ public class genlist<T>{
 	public int size => data.Length; // property
 	public T this[int i] => data[i]; // indexer
 	public genlist(){ data = new T[0]; }
-	public void add(T item){ /* add item to the list */
+	public void add(T item){ 
 		T[] newdata = new T[size+1];
 		System.Array.Copy(data,newdata,size);
 		newdata[size]=item;
 		data=newdata;
+	public void remove(int i){
+		T[] newdata = new T[size - 1]
+		System.Array.Copy(data,newdata,size);
+		for (int j = 0; j<size-1;j++){
+			if (j<i){
+				newdata[j] = data[j];
+			}
+			else {
+				newdata[j] = data[j+1];
+			}
+		}
+		data = newdata;
+	}
 	}
 }
