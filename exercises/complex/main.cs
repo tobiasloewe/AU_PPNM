2834c6888fade1d4bf5a07b7449c5eb85925f011
tobiasloewe <tobias.loewe@studium.uni-hamburg.de>
2025-02-24 11:40:55 +0100

    fix

--- exercises/complex/main.cs
+++ exercises/complex/main.cs
@@ -6,10 +6,10 @@ static int Main(){
     complex z1 = cmath.sqrt(-complex.One);
     complex expected1 = complex.I;
 
-    WriteLine($"√-1 approx i: {expected1.approx(z1)}");
+    WriteLine($"√-1 approx i: {expected1.approx(z1)||expected1.approx(-z1)}");
     WriteLine($"Computed: {z1}, Expected: {expected1}");
 
-    complex ztest = cmath.arg(cmath.arg(-complex.One));
+    complex ztest = cmath.arg(-complex.One);
     complex ztest2 = cmath.exp(complex.I*ztest/2);
     WriteLine($"arg of -1 is {ztest}, exp is {ztest2}");
     WriteLine($"COMMENT: somehow by default sqrt uses other solution");
