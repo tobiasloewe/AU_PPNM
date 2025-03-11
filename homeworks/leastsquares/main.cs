d75d8f98d2107ed5faf7a2259ee8e26e7861fc8d
tobiasloewe <tobias.loewe@studium.uni-hamburg.de>
2025-03-11 13:30:18 +0100

    now done fr

--- homeworks/leastsquares/main.cs
+++ homeworks/leastsquares/main.cs
@@ -9,9 +9,17 @@ class main{
     // init vars
     string[] labels = null;
     double[][] fitdata = null;
+    int n = 2; int m = 2;
 
     // IO
 	for (int i = 0; i < args.Length; i++) {
+        if (args[i] == "-dims" && i + 1 < args.Length) {
+            var values = args[i + 1].Split(',');
+            if (values.Length == 2){
+                n=int.Parse(values[0]);
+				m=int.Parse(values[1]);
+            }
+        }
         if (args[i] == "-fitdata" && i + 1 < args.Length){
             string filename = args[i+1];
             string[] lines = File.ReadAllLines(filename);
@@ -30,6 +38,21 @@ class main{
             }
         }
     }
+    
+    var rnd = new System.Random(1); 
+    var A = new matrix(n,m);
+    for (int i=0; i<n; i++){
+        for (int j=0; j<m; j++){
+            A.set(i,j,rnd.NextDouble());
+        }
+    }
+    (matrix Q, matrix R) = QR.decomp(A);
+    Error.WriteLine($"Tall {n} x {m} matrix:");
+    Error.WriteLine($"Is Q.T*Q = Id? {matrix.id(m).approx(Q.T*Q)}");
+    R.print("R should be upper triangular: ","{0,10:g3} ",System.Console.Error);
+
+
+
     double[] logfitdata = (double[])fitdata[1].Clone();
     double[] logfiterror = (double[])fitdata[2].Clone();
 
@@ -58,9 +81,8 @@ class main{
     for (int i = 0; i<fitdata[0].Length; i++){
         WriteLine($"{fitdata[0][i]} {fitdata[1][i]} {fitdata[2][i]} {fittedFunc(fitdata[0][i])} {fittedFunc1(fitdata[0][i])} {fittedFunc2(fitdata[0][i])} {fittedFunc3(fitdata[0][i])} {fittedFunc4(fitdata[0][i])}");
     }
-    WriteLine("\n");
-    WriteLine($"Half life: {Log(2)/par[1]:F4} +- {stds[1]/par[1]/par[1]*Log(2):F4} days");
-    WriteLine($"modern value: 3.6313 +- 0.0012 days");
-    cov.print("\nCov matrix: ");
+    Error.WriteLine($"Half life: {Log(2)/par[1]:F4} +- {stds[1]/par[1]/par[1]*Log(2):F4} days");
+    Error.WriteLine($"modern value: 3.6313 +- 0.0012 days");
+    cov.print("\nCov matrix: ", "{0,10:g3} ",System.Console.Error);
 }
 }
