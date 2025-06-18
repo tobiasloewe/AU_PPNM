10da25978492352d464598a9f132a0db03d7bcdd
tobiasloewe <tobias.loewe@studium.uni-hamburg.de>
2025-06-18 14:36:19 +0200

    ann stuff

--- homeworks/ann/main.cs
+++ homeworks/ann/main.cs
@@ -9,57 +9,110 @@ class Program
         int trainSamples = 1000;
         int valSamples = 300;
         int testSamples = 300;
+        string lossfile = null;
+        string plotdatafile = null;
+
+        for(int i=0;i<args.Length;i++){
+            string arg = args[i];
+            if(arg=="-samples" && i+1<args.Length) {
+                trainSamples=int.Parse(args[i+1]);
+                valSamples = trainSamples / 3;
+                testSamples = trainSamples / 3;
+            }
+            if(arg=="-lossfile" && i+1<args.Length) lossfile=args[i+1];
+            if(arg=="-plotdatafile" && i+1<args.Length) plotdatafile=args[i+1];
+        }   
+
         vector xtrain = new vector(trainSamples);
         vector ytrain = new vector(trainSamples);
         vector xval = new vector(valSamples);
         vector yval = new vector(valSamples);
+
         vector xtest = new vector(testSamples);
         vector ytest = new vector(testSamples);
-        
+        vector ytestprime = new vector(testSamples);
+        vector ytestdoubleprime = new vector(testSamples);
 
-        // Suggestion: Use a smaller learning rate, normalize input, and initialize weights carefully.
-        // Also, try increasing the number of neurons or epochs if underfitting.
+        // Define the function g and its derivatives
         Func<double, double> g = x => Math.Cos(5 * x - 1) * Math.Exp(-x * x);
+        Func<double, double> g_prime = x => -2 * x * Math.Cos(5 * x - 1) * Math.Exp(-x * x)
+                            - 5 * Math.Sin(5 * x - 1) * Math.Exp(-x * x);
+        Func<double, double> g_doubleprime = x =>
+            (4 * x * x - 2) * Math.Cos(5 * x - 1) * Math.Exp(-x * x)
+            + (20 * x * Math.Sin(5 * x - 1) - 25 * Math.Cos(5 * x - 1)) * Math.Exp(-x * x);
 
-        // Normalize input data to mean 0, std 1 (if not already)
-        // Consider using a smaller learning rate in your ANN class (e.g., 0.01 or 0.001)
-        // Try more neurons (e.g., ANN myNetwork = new ANN(10);)
-        // Optionally, add regularization or early stopping
-        Random rand = new Random();
         for (int i =0; i < trainSamples; i++)
         {
-            xtrain[i] = rand.NextDouble() * 2 - 1; 
+            xtrain[i] = (2.0/trainSamples)*i - 1.0; 
             ytrain[i] = g(xtrain[i]); // Generate y values using the function g
         }
         for (int i =0; i < valSamples; i++)
         {
-            xval[i] = rand.NextDouble() * 2 - 1; 
+            xval[i] = (2.0/valSamples)*i - 1.0; 
             yval[i] = g(xval[i]); // Generate y values using the function g
         }
         for (int i =0; i < testSamples; i++)
         {
-            xtest[i] = rand.NextDouble() * 2 - 1; 
-            ytest[i] = g(xtest[i]); // Generate y values using the function g
+            xtest[i] = (2.0/testSamples)*i - 1.0; 
+            ytest[i] = g(xtest[i]);
+            ytestprime[i] = g_prime(xtest[i]);
+            ytestdoubleprime[i] = g_doubleprime(xtest[i]);
         }
-        // Train the network
-        ANN myNetwork = new ANN(4, xtrain, ytrain);
 
-        int epochs = 2;
+        // Train the network
+        int nNeurons = 20; // Number of neurons in the hidden layer
+        int epochs = 500;
         double maxLoss = 10.0;
-        double minLoss = 0.01;
-        for (int epoch = 0; epoch < epochs; epoch++)
+        double minLoss = 0.1;
+        double lrate = 0.01;
+        vector initweights = new vector(1.0,1.0,1.0);
+
+        Console.WriteLine($"Training parameters:");
+        Console.WriteLine($" neurons = {nNeurons}");
+        Console.WriteLine($"  epochs   = {epochs}");
+        Console.WriteLine($"  maxLoss  = {maxLoss}");
+        Console.WriteLine($"  minLoss  = {minLoss}");
+        Console.WriteLine($"  lrate    = {lrate}");
+        Console.WriteLine($"  trainSamples = {trainSamples}");
+        Console.WriteLine($"  valSamples   = {valSamples}");
+        Console.WriteLine($"  testSamples  = {testSamples}");
+        
+        ANN myNetwork = new ANN(nNeurons, xtrain, ytrain, lrate, "gausswave", initweights);
+
+        if (lossfile != null)
         {
-            myNetwork.train();
-            double trainLoss = myNetwork.currentLoss();
-            double valLoss = myNetwork.currentLoss(xval, yval);
-            Console.WriteLine($"Epoch {epoch + 1}\ntrain loss: {trainLoss}  val loss: {valLoss}");
-            if (trainLoss < minLoss || trainLoss > maxLoss || double.IsNaN(trainLoss))
+            using (var writer = new System.IO.StreamWriter(lossfile))
             {
-                Console.WriteLine($"Training stopped at epoch {epoch + 1}");
-                double testLoss = myNetwork.currentLoss(xtest, ytest);
-                Console.WriteLine($"Test loss: {testLoss}");
-                break;
+                Console.WriteLine($"Writing loss to {lossfile}");
+                for (int epoch = 0; epoch < epochs; epoch++)
+                {
+                    myNetwork.train();
+                    double trainLoss = myNetwork.currentLoss();
+                    double valLoss = myNetwork.currentLoss(xval, yval);
+                    writer.WriteLine($"{epoch + 1} {trainLoss} {valLoss}");
+                    if (trainLoss < minLoss || trainLoss > maxLoss || double.IsNaN(trainLoss))
+                    {
+                        Console.WriteLine($"Training stopped at epoch {epoch + 1}");
+                        double testLoss = myNetwork.currentLoss(xtest, ytest);
+                        Console.WriteLine($"Test loss: {testLoss}");
+                        break;
+                    }
+                }
             }
         }
+        if (plotdatafile != null)
+        {
+            using (var writer = new System.IO.StreamWriter(plotdatafile))
+            {
+                Console.WriteLine($"Writing plot data to {plotdatafile}");
+                for (int i = 0; i < testSamples; i++)
+                {
+                    double ypred = myNetwork.response(xtest[i]);
+                    double ypredprime = myNetwork.firstDerivative(xtest[i]);
+                    double ypreddoubleprime = myNetwork.secondDerivative(xtest[i]);
+                    writer.WriteLine($"{xtest[i]} {ytest[i]} {ypred} {ytestprime[i]} {ypredprime} {ytestdoubleprime[i]} {ypreddoubleprime}"); 
+                }
+            }
         }
+}
 }
--- homeworks/libraries/ann.cs
+++ homeworks/libraries/ann.cs
@@ -2,39 +2,129 @@ using System;
 using static System.Math;
 
 public class ANN{
-    private int n; /* number of hidden neurons */
-    private Func<double,double> f = x => x*Exp(-x*x); /* activation function */
-    private vector pars; /* network parameters */
+    private int n;                                      /* number of hidden neurons */
+    private Func<double,double> activation;   /* activation function */
+    private vector pars_;
+    private double learnRate;                                /* network parameters */
+    public readonly vector x;
+    public readonly vector y;
+    public static Func<vector, vector, double> mseLoss = (vector x, vector y) => {
+        double loss = 0.0;
+        for(int i=0;i<x.size;i++){
+            double d=y[i]-x[i];
+            loss += d*d;
+        }
+        return loss/x.size;
+    };
 
-    public ANN(int n){
-        this.n=n; this.pars=new vector(n*3);
+    public ANN(int n, vector xinit, vector yinit, double initialLearnRate = 0.1, string acti = "gausswave", vector defaultPars = null){
+        if (defaultPars == null) defaultPars = new vector(1.0,1.0,1.0);
+
+        if (acti == "tanh") this.activation = (double x) => { return (1.0 - Exp(-2*x))/(1.0 + Exp(-2*x)); };
+        else if (acti == "sigmoid") this.activation = (double x) => { return 1.0/(1.0 + Exp(-x)); };
+        else if (acti == "relu") this.activation = (double x) => { return Max(0,x); };
+        else if (acti == "linear") this.activation = (double x) => { return x; };
+        else if (acti == "gausswave") this.activation = (double x) => {return x*Exp(-x*x);};
+        else throw new ArgumentException("Unknown activation function");
+
+        this.n=n; this.pars_=new vector(n*3);
+        this.x=xinit; this.y=yinit;
+        this.learnRate=initialLearnRate;
+        var rand = new Random();
         for (int i=0;i<this.n;i++){
-            this.pars[3*i]=1.0; /* input scaling */
-            this.pars[1+3*i]=1.0; /* output scaling */
-            this.pars[2+3*i]=1.0; /* weight */
+            this.pars_[3*i]=defaultPars[0]*2*rand.NextDouble()-1; /* input scaling */
+            this.pars_[1+3*i]=defaultPars[1]*2*rand.NextDouble()-1; /* output scaling */
+            this.pars_[2+3*i]=defaultPars[2]*2*rand.NextDouble()-1; /* weight */
         }
     }
-    public double response(double x){
-      /* return the response of the network to the input signal x */
+    public double response(double x, vector pars = null){
+        if (pars == null) pars = this.pars_;
         double yguess =0.0;
         for (int i=0;i<this.n;i++){
-            yguess+=f((x-this.pars[3*i])/this.pars[1+3*i])*this.pars[2+3*i];
+            yguess+=activation((x-pars[3*i])/pars[1+3*i])*pars[2+3*i];
         }
         return yguess;
     }
-    public void train(vector x,vector y){
-        /* train the network to interpolate the given table {x,y} */
-        Func<vector,double> f = (vector input) => this.mseCost(input, y); /* activation function */
-
-        var (newpars, steps) = Min.newton(f,this.pars,1e-3,1000);
-        this.pars=newpars;
-    }
-    double mseCost(vector x,vector y){
-        double c=0;
-        for (int i=0;i<x.size;i++){
-            double d=y[i]-response(x[i]);
-            c+=d*d;
+    public vector response(vector x, vector pars = null){
+        if (pars == null) pars = this.pars_;
+        vector yguess = new vector(x.size);
+        for (int i=0;i<this.n;i++){
+            yguess[i] = response(x[i], pars);
         }
-        return c/x.size;
+        return yguess;
+    }
+    public double currentLoss(vector pars = null){
+        if (pars == null) pars = this.pars_;
+        return ANN.mseLoss(this.response(this.x, pars), this.y);
+    }
+    public void train(int n = 1)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            vector gradVal = gradient(this.currentLoss, this.pars_);
+        gradVal /= gradVal.norm(); // Normalize the gradient
+        this.pars_ -= this.learnRate * gradVal;
+        gradVal.print("Gradient");
+        }
+    }
+    public double currentLoss(vector x, vector y){
+        return ANN.mseLoss(this.response(x), y);
+    }
+    private static vector gradient(Func<vector, double> φ, vector x)
+    {
+        double φx = φ(x); // Evaluate φ at the current point
+        vector gφ = new vector(x.size); // Initialize gradient vector
+
+        for (int i = 0; i < x.size; i++)
+        {
+            double dxi = Max(1e-8, Abs(x[i]) * 1e-5); // Relative step size
+            vector xCopy = x.copy();
+            xCopy[i] += dxi;
+            gφ[i] = (φ(xCopy) - φx) / dxi; // Compute partial derivative
+        }
+
+        return gφ; // Return the gradient vector
+    }
+    public void setLearnRate(double newLearnRate)
+    {
+        this.learnRate = newLearnRate;
+    }
+    public double firstDerivative(double x, vector pars = null) {
+        if (pars == null) pars = this.pars_;
+        double result = 0.0;
+        for (int i = 0; i < this.n; i++) {
+            double a = pars[3 * i];       // Output scaling
+            double b = pars[3 * i + 1];   // Input scaling
+            double c = pars[3 * i + 2];   // Weight
+            double z = b * x + c;
+            result += a * (Exp(-z * z) - 2 * z * z * Exp(-z * z)) * b; // φ'(z) * b
+        }
+        return result;
+    }
+
+    public double secondDerivative(double x, vector pars = null) {
+        if (pars == null) pars = this.pars_;
+        double result = 0.0;
+        for (int i = 0; i < this.n; i++) {
+            double a = pars[3 * i];       // Output scaling
+            double b = pars[3 * i + 1];   // Input scaling
+            double c = pars[3 * i + 2];   // Weight
+            double z = b * x + c;
+            result += a * (-6 * z * Exp(-z * z) + 4 * z * z * z * Exp(-z * z)) * b * b; // φ''(z) * b^2
+        }
+        return result;
+    }
+
+    public double antiDerivative(double x, vector pars = null) {
+        if (pars == null) pars = this.pars_;
+        double result = 0.0;
+        for (int i = 0; i < this.n; i++) {
+            double a = pars[3 * i];       // Output scaling
+            double b = pars[3 * i + 1];   // Input scaling
+            double c = pars[3 * i + 2];   // Weight
+            double z = b * x + c;
+            result += a * (-0.5 * Exp(-z * z)) / b; // Φ(z) / b
+        }
+        return result;
     }
 }
